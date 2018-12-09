3e2c748e6fb738036843c0de1353ec6cea1a48ae
root <sirthesurfer@gmail.com>
2018-12-09 12:16:51 +0000

    Sunshine initial code

---
+++ src/main/java/com/example/android/sunshine/sync/retrofit/RecipeClient.java
@@ -0,0 +1,20 @@
+/*
+package com.example.android.sunshine.sync.retrofit;
+
+import com.example.user.bakingapp.model.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RecipeClient {
+
+    String RECIPE_URL = "https://d17h27t6h515a5.cloudfront.net/";
+
+    @SuppressWarnings("SpellCheckingInspection")
+    @GET("topher/2017/May/59121517_baking/baking.json")
+    Call<List<Recipe>> getRecipes();
+
+}
+*/
